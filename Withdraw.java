a8c29f201b0c4c7110a883e04bebfd5cee642413
Ankita53744 <223ankita0075@dbit.in>
2025-06-23 21:58:27 +0530

    Add files via upload

---
+++ Withdraw.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+
+class Withdraw extends JFrame
+{
+    Withdraw(String username)
+    {
+        Font f = new Font("Futura", Font.BOLD, 40);
+        Font f2 = new Font("Calibri", Font.PLAIN, 22);
+
+        JLabel title = new JLabel("Withdraw Money", JLabel.CENTER);
+        JLabel label = new JLabel("Enter Amount:");
+        JTextField t1 = new JTextField(10);
+        JButton b1 = new JButton("Withdraw");
+        JButton b2 = new JButton("Back");
+
+        title.setFont(f);
+        label.setFont(f2);
+        t1.setFont(f2);
+        b1.setFont(f2);
+        b2.setFont(f2);
+
+        Container c = getContentPane();
+        c.setLayout(null);
+
+        title.setBounds(200, 30, 400, 50);
+        label.setBounds(250, 120, 300, 30);
+        t1.setBounds(250, 160, 300, 30);
+        b1.setBounds(300, 220, 200, 40);
+        b2.setBounds(300, 280, 200, 40);
+
+        c.add(title);
+        c.add(label);
+        c.add(t1);
+        c.add(b1);
+        c.add(b2);
+
+
+
+        setVisible(true);
+        setSize(800, 550);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Withdraw Money");
+    }
+
+    public static void main(String[] args) {
+        new Withdraw("sam");
+    }
+}
